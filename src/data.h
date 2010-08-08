c501d1987229c4b0bdddbabbd7e7096f229db0c8
Alessandro Ghedini <al3xbio@gmail.com>
2010-08-08 20:23:44 +0200

    add explosion frames

--- src/data.h
+++ src/data.h
@@ -1,19 +1,35 @@
 /* Allegro datafile object indexes, produced by grabber v4.2.2, Unix */
 /* Datafile: /home/ale/Sources/Games/SpaceShooter/data/SpaceShooter.dat */
-/* Date: Sun Aug  8 19:13:41 2010 */
+/* Date: Sun Aug  8 20:37:15 2010 */
 /* Do not hand edit! */
 
 #define BMP_BACKGROUND                   0        /* BMP  */
-#define BMP_SPACESHIP                    1        /* BMP  */
+#define BMP_BULLET                       1        /* BMP  */
 #define BMP_ENEMY1                       2        /* BMP  */
 #define BMP_ENEMY2                       3        /* BMP  */
 #define BMP_ENEMY3                       4        /* BMP  */
-#define BMP_BULLET                       5        /* BMP  */
-#define BMP_ROCKET                       6        /* BMP  */
-#define SND_PAUSE                        7        /* SAMP */
+#define BMP_ROCKET                       5        /* BMP  */
+#define BMP_SPACESHIP                    6        /* BMP  */
+#define FONT_YANONE                      7        /* FONT */
 #define SND_EXPLOSION                    8        /* SAMP */
 #define SND_FIRE                         9        /* SAMP */
-#define SND_ROCKET                       10       /* SAMP */
-#define SND_GAMEOVER                     11       /* SAMP */
-#define FONT_YANONE                      12       /* FONT */
+#define SND_GAMEOVER                     10       /* SAMP */
+#define SND_PAUSE                        11       /* SAMP */
+#define SND_ROCKET                       12       /* SAMP */
+#define ANI_EXPLOSION_0                  13       /* BMP  */
+#define ANI_EXPLOSION_1                  14       /* BMP  */
+#define ANI_EXPLOSION_2                  15       /* BMP  */
+#define ANI_EXPLOSION_3                  16       /* BMP  */
+#define ANI_EXPLOSION_4                  17       /* BMP  */
+#define ANI_EXPLOSION_5                  18       /* BMP  */
+#define ANI_EXPLOSION_6                  19       /* BMP  */
+#define ANI_EXPLOSION_7                  20       /* BMP  */
+#define ANI_EXPLOSION_8                  21       /* BMP  */
+#define ANI_EXPLOSION_9                  22       /* BMP  */
+#define ANI_EXPLOSION_10                 23       /* BMP  */
+#define ANI_EXPLOSION_11                 24       /* BMP  */
+#define ANI_EXPLOSION_12                 25       /* BMP  */
+#define ANI_EXPLOSION_13                 26       /* BMP  */
+#define ANI_EXPLOSION_14                 27       /* BMP  */
+#define ANI_EXPLOSION_15                 28       /* BMP  */
 
