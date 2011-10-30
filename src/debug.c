452f8aeca442f23e62ec5ca28d940558f7647933
Alessandro Ghedini <al3xbio@gmail.com>
2011-10-30 21:56:41 +0100

    add debug.c

--- src/debug.c
+++ src/debug.c
@@ -1,3 +1,5 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 /*
     This file is part of SpaceShooter.
     Copyright (C) 2010 Alessandro Ghedini <al3xbio@gmail.com>
@@ -30,4 +32,62 @@ void printd(char* format, ...) {
 		va_end(args);
 		printf("\n");
 	}
+=======
+=======
+>>>>>>> Stashed changes
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define COLOR_GREEN      "[1;32m"
+#define COLOR_YELLOW     "[1;33m"
+#define COLOR_RED        "[1;31m"
+#define COLOR_OFF        "[0m"
+
+void ok_printf(const char *fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr, "[" COLOR_GREEN "ok" COLOR_OFF "] ");
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+}
+
+void debug_printf(const char *fmt, ...) {
+#ifndef DEBUG
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr,  "[" COLOR_YELLOW "db" COLOR_OFF "] ");
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+#endif
+}
+
+void err_printf(const char *fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr,  "[" COLOR_RED "no" COLOR_OFF "] ");
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+}
+
+void fail_printf(const char *fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr,  "[" COLOR_RED "no" COLOR_OFF "] ");
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+
+	exit(-1);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 }
